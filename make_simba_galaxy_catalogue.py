61f43c833c00ccc500c08a9e1ef9b3d72857ce23
Fergus Cullen <fcullen@ferguss-mbp-2.home>
2020-10-08 12:56:18 +0100

    added photometry in cosmos filters to the catalogues

--- make_simba_galaxy_catalogue.py
+++ make_simba_galaxy_catalogue.py
@@ -1,47 +1,67 @@
 import caesar
 import numpy as np
 
-ofile = open('moons_simba_galinfo.dat', 'w')
-ofile.write('# idx lmass_stellar lmass_tot lsfr lssfr lzstar\n')
+def make_catalogue(output_file, sanpshot_files):
 
-# load the snapshot:
-hdf_files = ['m100n1024_090.hdf5', 'm100n1024_104.hdf5']
+    ofile = open(output_file, 'w')
+    ofile.write('# idx lmass_stellar lmass_tot lsfr lssfr lzstar ')
+    ofile.write('u_cfht b_subaru v_subaru g_subaru r_subaru i_subaru z_subaru ')
+    ofile.write('y_vista j_vista h_vista k_vista irac_1 irac_2\n')
 
-for hdf in hdf_files:
+    for hdf in sanpshot_files:
 
-    obj = caesar.load(hdf)
-    print("Loded simba snapshot at z={:.2f}".format(obj.simulation.redshift))
+        obj = caesar.load(hdf)
+        print("Loded simba snapshot at z={:.2f}".format(obj.simulation.redshift))
 
-    # select the galaxies:
-    galaxies = obj.galaxies
+        # select the galaxies:
+        galaxies = obj.galaxies
 
-    # loop though galaxies and extract information:
-    for i, gal in enumerate(galaxies[:]):
+        # loop though galaxies and extract information:
+        for i, gal in enumerate(galaxies[:]):
 
-        ofile.write('{:^15d}'.format(i))
+            ofile.write('{:^15d}'.format(i))
 
-        # stellar mass
-        lmass_stellar = np.log10(gal.masses['stellar'])
-        ofile.write('{:15.3f}'.format(lmass_stellar))
+            # stellar mass
+            lmass_stellar = np.log10(gal.masses['stellar'])
+            ofile.write('{:15.3f}'.format(lmass_stellar))
 
-        # total mass:
-        lmass_tot = np.log10(gal.masses['total'])   
-        ofile.write('{:15.3f}'.format(lmass_tot))
+            # total mass:
+            lmass_tot = np.log10(gal.masses['total'])   
+            ofile.write('{:15.3f}'.format(lmass_tot))
 
-        # sfr and ssfr:
-        sfr = gal.sfr.value
-        if sfr <= 0:
-            lsfr = -999.
-            lssfr = -999.
-        else:
-            lsfr = np.log10(sfr)
-            lssfr = np.log10(sfr / gal.masses['stellar'].value)
-        
-        ofile.write('{:15.3f}'.format(lsfr))
-        ofile.write('{:15.3f}'.format(lssfr))
+            # sfr and ssfr:
+            sfr = gal.sfr.value
+            if sfr <= 0:
+                lsfr = -999.
+                lssfr = -999.
+            else:
+                lsfr = np.log10(sfr)
+                lssfr = np.log10(sfr / gal.masses['stellar'].value)
+            
+            ofile.write('{:15.3f}'.format(lsfr))
+            ofile.write('{:15.3f}'.format(lssfr))
 
-        # metallicity:
-        lzstar = np.log10(gal.metallicities['stellar'])
-        ofile.write('{:15.3f}\n'.format(lzstar))
+            # metallicity:
+            lzstar = np.log10(gal.metallicities['stellar'])
+            ofile.write('{:15.3f}'.format(lzstar))
 
-ofile.close()
+            # add photometry in the COSMOS filters (narrowbands not available)
+            filter_names = ['megacam_u', 'suprimecam_b', 'suprimecam_v', 'suprimecam_g', 
+                'suprimecam_r', 'suprimecam_i', 'suprimecam_z', 'vista_y', 'vista_j',
+                'vista_h', 'vista_k', 'irac_1', 'irac_2']
+
+            for filt in filter_names:
+
+                ofile.write('{:15.2f}'.format(gal.appmag[filt]))
+
+            ofile.write('\n')
+
+    ofile.close()
+
+if __name__ == "__main__":
+
+    make_catalogue(output_file='moons_simba_z1p5_galaxies.cat', 
+        sanpshot_files=['./simba_snapshots/m100n1024_085.hdf5', 
+                        './simba_snapshots/m100n1024_090.hdf5', 
+                        './simba_snapshots/m100n1024_098.hdf5'])
+
